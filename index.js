719868f17ca46227e5867538799c50b879099f13
Levy Wechuli <levy.wechuli@student.moringaschool.com>
2024-10-22 19:13:01 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,25 +1,63 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // Compare the word with its reversed version
+  return word === word.split('').reverse().join('');
 }
 
-/* 
-  Add your pseudocode here
-*/
 
-/*
-  Add written explanation of your solution here
-*/
 
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+  //  here is Custom tests
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
 
   console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  console.log("=>", isPalindrome("robot")); 
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba")); 
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a")); 
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("ab")); 
 }
 
 module.exports = isPalindrome;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
